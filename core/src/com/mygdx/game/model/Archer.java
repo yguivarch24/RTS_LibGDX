1487a878541bc4089c983075962d7c8a66b9db65
jdides <justindides@etu.enseeiht.fr>
2019-05-09 16:30:12 +0200

    Correction de soldat et ajotu d'archer

---
+++ core/src/com/mygdx/game/model/Archer.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.model;
+
+public class Archer extends Unite {
+	
+    public Archer(int x, int y, Joueur joueur) {
+        super(x, y, "Archer", joueur, 7, 7, 3, 1, 5, 4, 4);
+    }
+
+}
