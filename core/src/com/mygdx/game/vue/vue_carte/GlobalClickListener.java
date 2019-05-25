55dd1b76cee14442d1c5bfa4237611af66831973
estellesmagge <estellesmagge@macbook-air-de-estelle.home>
2019-05-25 15:31:04 +0200

    Global Clicklistener pour de vrai

---
+++ core/src/com/mygdx/game/vue/vue_carte/GlobalClickListener.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.vue.vue_carte;
+
+import com.mygdx.game.model.*;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class GlobalClickListener extends ClickListener {
+	
+	static protected LiveObject ObjetSelec;
+	
+	public void clicked(InputEvent event, float x, float y) {}
+}
