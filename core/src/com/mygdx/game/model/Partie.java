795ab5746295b389e3c5c2cabc43e3999d5b0f14
ggermon <gabrielle.germon@etu.enseeiht.fr>
2019-05-26 15:49:30 +0200

    changement de la position des hotels de ville

--- core/src/com/mygdx/game/model/Partie.java
+++ core/src/com/mygdx/game/model/Partie.java
@@ -8,42 +8,39 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/** Classe représentant une partie du jeu */
+
 public class Partie {
 	
 	/** Nombre de joueurs dans la partie */ 
-	private int nbJoueur;
-	
+	private int nbJoueur;	
 	
 	/** Liste des joueurs dans la partie */ 
 	private List<Joueur> listeJoueurs;
 	
-	/** N� D'index du joueur courant dans la liste. */
+	/** Index du joueur courant dans la liste. */
 	private int indexJoueurCourant;
+	
 	/** Joueur courant dont c'est le tour de jouer. */
 	private Joueur joueurCourant;
 	
 	/** Carte de la partie */
 	private Carte carte;
 	
-	/** Num�ro du tour de jeu courant */
+	/** Numéro du tour de jeu courant */
 	private int nbTour;
 	
 	
 	/** Cosntructuer de la partie. Initialise le nombre de joueurs et la lsite de joueurs.
-	 * 
 	 * @param nbJ Nombre de joueurs de la partie.
 	 * @param tailleCarte la taille de la carte
 	 */
 	public Partie(int nbJ, int tailleCarte) {
 		nbJoueur = nbJ;
-		listeJoueurs = new ArrayList(nbJ);
+		listeJoueurs = new ArrayList<>(nbJ);
 		carte = new Carte(tailleCarte);
 		indexJoueurCourant = 1;
 		nbTour = 1;
-	}
-	
-	public void tourDeJeu() {
-		
 	}
 	
 	/** Permet d'initialiser la partie : hdv de chaque joueur. */
@@ -54,17 +51,8 @@ public class Partie {
 		this.ajouterJoueur(j1);
 		this.ajouterJoueur(j2);
 		this.joueurCourant = j1;
-		/*listeJoueurs.forEach(j -> {
-			 // Cr�ation de l'hotel de ville
-			try {
-				HotelDeVille hdv = new HotelDeVille(1,2, carte, j);
-				j.ajouterBatiment(hdv);
-			} catch (CaseOccupeeException e1) {
-				System.out.println("Pas de place pour l'HDV \n");
-			} 
-		});*/
 		try {
-			HotelDeVille hdv1 = new HotelDeVille(6,7, carte, j1);
+			HotelDeVille hdv1 = new HotelDeVille(41,38, carte, j1);
 			j1.ajouterBatiment(hdv1);
 			HotelDeVille hdv2 = new HotelDeVille(7,8, carte, j2);
 			j2.ajouterBatiment(hdv2);
@@ -73,16 +61,16 @@ public class Partie {
 		} 
 	}
 	
-	/** Permet d'ajouter un joueur � la partie. (Normalement uniquement utiliser lors de la cr�ation de la partie)
-	 * 
-	 * @param newJoueur Le joueur � ajouter � la partie.
+	/** Permet d'ajouter un joueur à la partie. 
+	 * (Normalement uniquement utilisé lors de la création de la partie)
+	 * @param newJoueur Le joueur à ajouter à la partie.
 	 */
 	public void ajouterJoueur(Joueur newJoueur) {
 		listeJoueurs.add(newJoueur);
 	}
 	
-	/** Permet de d�finir quel joueur doit jouer. Le tour des joueurs suit un ordre d�finit
-	 * lors de la cr�ation de la partie par les joueurs.
+	/** Permet de définir quel joueur doit jouer. Le tour des joueurs suit un ordre définit
+	 * lors de la création de la partie par les joueurs.
 	 */
 	public void joueurSuivant() {
 		if(indexJoueurCourant >= nbJoueur) {
@@ -93,34 +81,36 @@ public class Partie {
 	}
 	
 	/** Permet d'obtenir le joueur courant. 
-	 * @return Le joueur courant
-	 * */
+	 * @return le joueur courant
+	 */
 	public Joueur getJoueur() {
 		return joueurCourant;
 	}
 	
 	/** Permet d'obtenir la liste de joueurs 
-	 * @return Les joueurs
-	 * */
+	 * @return la liste des joueurs
+	 */
 	public List<Joueur> getJoueurs() {
 		return listeJoueurs;
 	}
 	
-	/** Permet d'obtenir le num�ro de tour de jeu courant.
-	 * @return le num�ro de tour. 
-	 *  */
+	/** Permet d'obtenir le numéro de tour de jeu courant.
+	 * @return le numéro de tour. 
+	 */
 	public int getNbTour() {
 		return nbTour;
 	}
 	
-	/** Getter de la carte */
+	/** Obtenir la carte
+	 * @return la carte
+	 */
 	public Carte getCarte() {
 		return carte;
 	}
 	
-	/** Setter de la carte 
-	 * @param c la carte
-	 * */
+	/** Changer la carte 
+	 * @param la nouvelle carte carte
+	 */
 	public void setCarte(Carte c) {
 		this.carte = c;
 	}
